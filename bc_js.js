d5b30c7ac9ae2d8b96fda0b3307478045a32b5c8
miloszmysliwiec <miszelangelo@gmail.com>
2018-11-09 13:19:01 +0100

    wbpage v2

--- bc_js.js
+++ bc_js.js
@@ -152,6 +152,84 @@ function showPhotos_SM(m_SM) {
   captionText_SM.innerHTML = dot_SM[photoIndex_SM-1].alt;
 }
 
+//WC
+
+function openModal_WC() {
+  document.getElementById('myModal_WC').style.display = "block";
+}
+
+function closeModal_WC() {
+  document.getElementById('myModal_WC').style.display = "none";
+}
+
+var photoIndex_WC = 1;
+showPhotos_WC(photoIndex_WC);
+
+function plusPhotos_WC(m_WC) {
+  showPhotos_WC(photoIndex_WC += m_WC);
+}
+
+function currentPhoto_WC(m_WC) {
+  showPhotos_WC(photoIndex_WC = m_WC);
+}
+
+function showPhotos_WC(m_WC) {
+  var k_WC;
+  var photos_WC = document.getElementsByClassName("myPhotos_WC");
+  var dot_WC = document.getElementsByClassName("demo_WC");
+  var captionText_WC = document.getElementById("caption_WC");
+  if (m_WC > photos_WC.length) {photoIndex_WC = 1}
+  if (m_WC < 1) {photoIndex_WC = photos_WC.length}
+  for (k_WC = 0; k_WC < photos_WC.length; k_WC++) {
+      photos_WC[k_WC].style.display = "none";
+  }
+  for (k_WC = 0; k_WC < dot_WC.length; k_WC++) {
+     dot_WC[k_WC].className = dot_WC[k_WC].className.replace(" active", "");
+  }
+  photos_WC[photoIndex_WC-1].style.display = "block";
+ dot_WC[photoIndex_WC-1].className += " active";
+  captionText_WC.innerHTML = dot_WC[photoIndex_WC-1].alt;
+}
+
+//BP
+
+function openModal_BP() {
+  document.getElementById('myModal_BP').style.display = "block";
+}
+
+function closeModal_BP() {
+  document.getElementById('myModal_BP').style.display = "none";
+}
+
+var photoIndex_BP = 1;
+showPhotos_BP(photoIndex_BP);
+
+function plusPhotos_BP(m_BP) {
+  showPhotos_BP(photoIndex_BP += m_BP);
+}
+
+function currentPhoto_BP(m_BP) {
+  showPhotos_BP(photoIndex_BP = m_BP);
+}
+
+function showPhotos_BP(m_BP) {
+  var k_BP;
+  var photos_BP = document.getElementsByClassName("myPhotos_BP");
+  var dot_BP = document.getElementsByClassName("demo_BP");
+  var captionText_BP = document.getElementById("caption_BP");
+  if (m_BP > photos_BP.length) {photoIndex_BP = 1}
+  if (m_BP < 1) {photoIndex_BP = photos_BP.length}
+  for (k_BP = 0; k_BP < photos_BP.length; k_BP++) {
+      photos_BP[k_BP].style.display = "none";
+  }
+  for (k_BP = 0; k_BP < dot_BP.length; k_BP++) {
+     dot_BP[k_BP].className = dot_BP[k_BP].className.replace(" active", "");
+  }
+  photos_BP[photoIndex_BP-1].style.display = "block";
+ dot_BP[photoIndex_BP-1].className += " active";
+  captionText_BP.innerHTML = dot_BP[photoIndex_BP-1].alt;
+}
+
 
 //tab price & timetable
 
